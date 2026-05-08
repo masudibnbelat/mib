b4bee3ff80c36b2a5a7778742f3b64b7a8969392
fenrirqutrub <fenrirqutrub@gmail.com>
2026-05-08 22:26:04 +0600

    first commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import SocialMedia from "./SocialMedia";
 const Footer = () => {
   return (
     <footer className="mb-16 lg:my-5">
-      <div className="flex items-center justify-center lg:justify-between gap-5  border-t border-(--color-active-bg) py-5 px-6">
+      <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between gap-5  border-t border-(--color-active-bg) py-5 px-6">
         <SocialMedia />
 
         <p className="text-[0.8rem] sm:text-[0.9rem] text-gray-600 rubik-regular whitespace-nowrap">
